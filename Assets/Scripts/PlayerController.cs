d9ed98c4ff9f40815ae631a6a9fe7ce859743964
Finn Leo <finn.elias.leo@gmail.com>
2023-12-19 16:36:48 +0100

    added obstacles & implemented simple Player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera camera;
+    [SerializeField] private NavMeshAgent agent;
 
-    // Update is called once per frame
-    void Update()
+    public void Move(InputAction.CallbackContext context)
     {
-        
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
     }
 }
