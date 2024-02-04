e6fcf747eb1735fb013a6468366547fb779e20aa
Finn Leo <finn.elias.leo@gmail.com>
2024-02-05 00:23:42 +0100

    added asset pack & started new game scene

---
+++ Assets/CameraBehavior.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    [SerializeField] private PlayerController Player;
+    [SerializeField] private Vector3 posOffset;
+
+    private void LateUpdate()
+    {
+        transform.position = Player.transform.position + posOffset;
+    }
+}
